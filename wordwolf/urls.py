3e6d15ebb3529a70a47479636bf3a835786d3b3c
1F10230178 <s1F102301785@iniad.org>
2025-12-12 12:02:45 +0900

    add: initial static html

--- wordwolf/urls.py
+++ wordwolf/urls.py
@@ -5,4 +5,5 @@ app_name = 'wordwolf'
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('game/', views.game, name='game'),
 ]
