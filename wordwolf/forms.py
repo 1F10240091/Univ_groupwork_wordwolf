3f2f3229859881ba2c742deef289449acc669367
1F10240091 <s1f102400910@iniad.org>
2025-12-26 23:10:54 +0900

    ログイン機能の実装: カスタムログインフォームの追加とログインページのUI改善

--- wordwolf/forms.py
+++ wordwolf/forms.py
@@ -1,6 +1,13 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User, Room, WordSet
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
 
 class SignUpForm(UserCreationForm):
     class Meta:
