68d743cd1f6ae6df3877aca548be212fd33452e0
1F10240091 <s1f102400910@iniad.org>
2025-12-26 23:16:54 +0900

    サインアップフォームのスタイルを改善: フィールドにクラスとプレースホルダーを追加し、UIを調整

--- wordwolf/forms.py
+++ wordwolf/forms.py
@@ -10,6 +10,12 @@ class LoginForm(AuthenticationForm):
             field.widget.attrs['placeholder'] = field.label
 
 class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1')
